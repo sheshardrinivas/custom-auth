52e1ebcf5b417621cd4bb75d7a694601fddd3880
sheshadrinivas <127858531+sheshardrinivas@users.noreply.github.com>
2025-12-14 15:47:58 +0530

    encoding type

--- app/api/password/route.ts
+++ app/api/password/route.ts
@@ -5,7 +5,7 @@ import { supabase } from "@/app/lib/supabase";
 export async function POST(request: Request) {
   const { username, password } = await request.json();
 
-  const hashedPassword = await argon2.hash(password);
+  const hashedPassword = await argon2.hash(password, { type: argon2.argon2id });
 
   const data = await supabase
     .from("auth")
