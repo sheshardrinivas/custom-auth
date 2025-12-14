2447236df2d4886ffa76df24cf2a0d9b58e84992
sheshadrinivas <127858531+sheshardrinivas@users.noreply.github.com>
2025-12-14 19:19:36 +0530

    made changes

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { useRouter } from "next/navigation";
-
+import Image from "next/image";
 export default function Admin() {
   const router = useRouter();
   function logout() {
@@ -13,14 +13,15 @@ export default function Admin() {
       });
   }
   return (
-    <div className="flex flex-col items-center justify-center h-screen w-screen gap-8">
-      admin page
-      <button
-        onClick={logout}
-        className=" border-2 border-stone-500 p-2 rounded-xl"
-      >
-        Logout
-      </button>
+    <div className="grid grid-cols-20 grid-rows-20 items-center justify-center h-screen w-screen gap-8">
+      <div className="bg-transparent row-end-1  col-span-2 grid grid-rows-1 grid-cols-1 items-center justify-center p-2">
+        <p
+          className="text-4xl text-gray-500 row-start-1 col-span-1 text-center rotate-180 "
+          onClick={logout}
+        >
+          ➣
+        </p>
+      </div>
     </div>
   );
 }
